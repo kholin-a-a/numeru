260374d3dfdc0b8e386bf299d2084ab5b038010e
kholin-a-a <kholin.a.a@mail.ru>
2021-05-21 13:20:12 +0300

    initial commit

---
+++ Numeru.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Numeru.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
