b3778c95c0a8d08f5847864f50482b48ff7e0b66
kholin-a-a <kholin.a.a@mail.ru>
2021-05-22 10:18:26 +0300

    index moment view

--- Numeru.Web/Controllers/MomentController.cs
+++ Numeru.Web/Controllers/MomentController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Numeru.Web.Controllers
 {
     public class MomentController: Controller
     {
+        public async Task<ActionResult> Index()
+        {
+            await Task.Yield();
 
+            return View();
+        }
     }
 }
