fdcf3ffbb228fcbf472f8f5eba395ea485a10301
kholin-a-a <kholin.a.a@mail.ru>
2021-05-24 14:09:34 +0300

    predictions initialization

--- Numeru.Web/Startup.cs
+++ Numeru.Web/Startup.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using Numeru.Services;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Numeru.Web
 {
@@ -25,6 +28,17 @@ namespace Numeru.Web
             services.AddScoped<IKindDefinder, NumberKindDefinder>();
             services.AddScoped<IDescriptionRepository, NumberDescriptionRepository>();
             services.AddScoped<IEvaluationAlgorithm<DateTime>, DateAlgorithm>();
+
+            var predictions = JsonConvert.DeserializeObject<IEnumerable<string>>(
+                File.ReadAllText(
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/predictions.json")
+                    )
+                );
+
+            services.AddScoped<IPredictionRepository>(sp =>
+                new InMemoryPredictionRepository(predictions)
+            );
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
