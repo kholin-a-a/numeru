10a16a6984beafafe11a8d599d162d510cc7996a
kholin-a-a <kholin.a.a@mail.ru>
2021-05-24 14:03:49 +0300

    InMemoryPredictionRepository

---
+++ Numeru.Services/Repositories/InMemoryPredictionRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Numeru.Services
+{
+    public class InMemoryPredictionRepository : IPredictionRepository
+    {
+        private readonly IEnumerable<string> _predictions;
+
+        public InMemoryPredictionRepository(IEnumerable<string> predictions)
+        {
+            this._predictions = predictions;
+        }
+
+        public IEnumerable<string> GetAll()
+        {
+            return this._predictions
+                .Select(s => s)
+                .ToList();
+        }
+    }
+}
