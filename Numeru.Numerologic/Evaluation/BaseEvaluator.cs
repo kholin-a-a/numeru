6d8e52febe462bbc47e6cf6d362d8f91fc796100
kholin-a-a <kholin.a.a@mail.ru>
2021-05-25 17:34:38 +0300

    added BaseEvaluator

---
+++ Numeru.Numerologic/Evaluation/BaseEvaluator.cs
@@ -0,0 +1,10 @@
+﻿namespace Numeru.Numerologic
+{
+    public class BaseEvaluator : AbstractEvaluator
+    {
+        protected override bool Done(Number number)
+        {
+            return number.IsBase();
+        }
+    }
+}
