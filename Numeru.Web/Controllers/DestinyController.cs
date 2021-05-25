633c95c3d64daefd00f4955ebd432b2cee7e3d71
kholin-a-a <kholin.a.a@mail.ru>
2021-05-25 08:52:31 +0300

    form validation

--- Numeru.Web/Controllers/DestinyController.cs
+++ Numeru.Web/Controllers/DestinyController.cs
@@ -4,9 +4,23 @@ namespace Numeru.Web.Controllers
 {
     public class DestinyController: Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return View(
+                new DestinyIndexViewModel()
+                );
+        }
+
+        [HttpPost]
+        public ActionResult Index(DestinyIndexViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            return View(vm);
         }
     }
 }
--- Numeru.Web/ViewModels/Destiny/DestinyIndexViewModel.cs
+++ Numeru.Web/ViewModels/Destiny/DestinyIndexViewModel.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Numeru.Web
 {
     public class DestinyIndexViewModel
     {
+        public DestinyIndexViewModel()
+        {
+            this.BirthDate = new DateTime(1970, 1, 1);
+        }
+
+        [Required(ErrorMessage = "Заполните, пожалуйста")]
+        [Display(Name = "Полное имя при рождении")]
+        [MaxLength(512)]
         public string Fullname { get; set; }
 
+        [Required(ErrorMessage = "Заполните, пожалуйста")]
+        [Display(Name = "Дата рождения")]
         public DateTime BirthDate { get; set; }
     }
 }
