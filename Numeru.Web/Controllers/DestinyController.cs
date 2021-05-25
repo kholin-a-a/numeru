1a5dbdb9a5ab1322c9069327ab715b7ad266dac9
kholin-a-a <kholin.a.a@mail.ru>
2021-05-25 08:15:54 +0300

    DestinyController

---
+++ Numeru.Web/Controllers/DestinyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Numeru.Web.Controllers
+{
+    public class DestinyController: Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
