5784784b94fbb814a5a1ce61c8ea86a4b09d0ead
kholin-a-a <kholin.a.a@mail.ru>
2021-05-27 12:28:09 +0300

    Create publish.ps1

---
+++ publish.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = "Stop"
+$version = "0.0.2"
+
+docker build -t kholinaa/numeru-web:$version .
+docker tag kholinaa/numeru-web:$version kholinaa/numeru-web:latest
+docker image push kholinaa/numeru-web:$version
+docker image push kholinaa/numeru-web:latest
