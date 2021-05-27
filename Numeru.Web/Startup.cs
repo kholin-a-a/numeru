2f5c4f199b8d759302cdb2f5991d314714facecc
kholin-a-a <kholin.a.a@mail.ru>
2021-05-27 11:54:58 +0300

    use http protocol only (without SSL)

--- Numeru.Web/Startup.cs
+++ Numeru.Web/Startup.cs
@@ -61,17 +61,9 @@ namespace Numeru.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
