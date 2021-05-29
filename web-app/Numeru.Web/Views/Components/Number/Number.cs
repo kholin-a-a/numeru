90405147aab4c3315b47dd869762755820d19054
kholin-a-a <kholin.a.a@mail.ru>
2021-05-29 19:55:04 +0300

    got rid of the _Number partial

---
+++ web-app/Numeru.Web/Views/Components/Number/Number.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Numeru.Web
+{
+    public class Number: ViewComponent
+    {
+        public IViewComponentResult Invoke(int number)
+        {
+            return View("~/Views/Components/Number/Number.cshtml", number);
+        }
+    }
+}
