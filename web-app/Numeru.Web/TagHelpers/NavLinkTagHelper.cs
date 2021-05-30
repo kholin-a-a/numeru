09dab98ad45013e1c896be23ce1a544648442ade
kholin-a-a <kholin.a.a@mail.ru>
2021-05-30 08:55:44 +0300

    NavLinkTagHelper

---
+++ web-app/Numeru.Web/TagHelpers/NavLinkTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace Numeru.Web
+{
+    [HtmlTargetElement("a")]
+    public class NavLinkTagHelper: TagHelper
+    {
+        public string RouteName { get; set; }
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(this.RouteName))
+                return;
+
+            if (this.RouteName.Equals(this.ViewContext.ViewData["Route"]))
+            {
+                output.AddClass("active", HtmlEncoder.Default);
+            }
+        }
+    }
+}
